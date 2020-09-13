85d2a1f9963390cf841cbdd71cbd92100f1d5eb1
Bikash Dhakal <dhakalbikash0@gmail.com>
2020-09-13 17:17:54 +0545

    css and font added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,39 @@
 import React from 'react'
 import { Jumbotron, Container, Button } from 'reactstrap'
 import Login from './Login'
+import Register from './Register'
 
-export default function Home(props) {
+
+export default function homepage(props) {
     return (
         <div>
-            <Jumbotron>
-            
-                <Container fluid>
-                <p className="lead">Be a life saver!!</p>
-                    <h1 className='display-3'>SERVE HUMANITY</h1>
-                    <p className="lead">through blood donation!!</p>
-                    <Login/>
-                    <p className="lead"> New user?
-                        
-                        <Button color="primary" onClick={() => props.history.push('/register')}>Register</Button>
-                        {/* <Link to='/register'>
-                            <button className="btn-primary">Register</button>
-                        </Link> */}
-                    </p>
-                </Container>
-           </Jumbotron>
+                    
+            {/* <!-- Header --> */}
+			<div id="header">
+				<div class="container">
+						
+					{/* <!-- Logo --> */}
+						<h1><a href="#" id="logo">SERVE HUMANITY</a></h1>
+
+					{/* <!-- Banner --> */}
+						<div id="banner">
+							<div class="container">
+								<section>
+									<header class="major">
+										<h2>Welcome to Serve Humanity!</h2>
+										<span class="byline"> Here you can save millions life by donating just single drop of your blood</span>
+                                        <h1>DONATE NOW</h1>
+									</header>
+									<Button color="primary" onClick={() => props.history.push('/login')}>Login</Button>
+                                    <Button color="primary" onClick={() => props.history.push('/register')}>Register</Button>
+								</section>	
+										
+							</div>
+						</div>
+
+				</div>
+			</div>
+           
         </div>
            
     )
